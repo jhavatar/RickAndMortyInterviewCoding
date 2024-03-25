5a8627105789e3fea3cf8cc03c83eac5a162f91d
jhavatar <jhdevaal@gmail.com>
2024-03-25 13:13:49 +0200

    cleanup version catalog and r8

--- data/rickmorty/src/main/java/io/chthonic/rickmortychars/data/rickmorty/rest/models/CharacterResults.kt
+++ data/rickmorty/src/main/java/io/chthonic/rickmortychars/data/rickmorty/rest/models/CharacterResults.kt
@@ -1,10 +1,14 @@
 package io.chthonic.rickmortychars.data.rickmorty.rest.models
 
+import androidx.annotation.Keep
 
+
+@Keep
 data class CharacterResults(
     val results: List<CharacterResult>
 )
 
+@Keep
 data class CharacterResult(
     val id: Int?,
     val name: String?,
