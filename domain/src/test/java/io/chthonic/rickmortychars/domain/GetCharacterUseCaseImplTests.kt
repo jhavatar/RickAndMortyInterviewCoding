81e1e69206c8800b48674cc20b57492750c3badc
jhavatar <jhdevaal@gmail.com>
2024-05-28 03:47:43 +0200

    test CI with a unit test and update CI JDK to 17

---
+++ domain/src/test/java/io/chthonic/rickmortychars/domain/GetCharacterUseCaseImplTests.kt
@@ -0,0 +1,12 @@
+package io.chthonic.rickmortychars.domain
+
+import junit.framework.TestCase.assertTrue
+import org.junit.Test
+
+class GetCharacterUseCaseImplTests {
+
+    @Test
+    fun test1() {
+        assertTrue(true)
+    }
+}
